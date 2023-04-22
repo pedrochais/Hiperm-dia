779beb8631b114103ef19bb7a82e4d6a70ad0b33
Pedro Rocha <pedrorocha.bou@gmail.com>
2023-04-22 15:46:41 -0300

    tarefa 4 finalizada

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Search:
         # Dicionario para armazenar indice invertido
         self.hash = {}
 
-    def createHash(self) -> dict:
+    def createHash(self) -> None:
         # Iterar sobre cada elemento <page>
         for child in self.root:
             # Recupera as informações id, título e texto do XML
@@ -39,6 +39,37 @@ class Search:
             
             # Cria uma lista de palavras contidas no título
             words = title.split(' ')
+            
+            # Itera sobre cada palavra do título
+            for word in words:
+                if word in self.hash:
+                    # Adiciona o objeto da página em uma lista que está associada a uma posição do hash
+                    self.hash[word].append(page)
+                else:
+                    # Cria uma lista associada a posição do hash e adiciona o primeiro objeto
+                    self.hash[word] = []
+                    self.hash[word].append(page)
+                    
+    def createHash_2(self) -> None:
+        # Iterar sobre cada elemento <page>
+        for child in self.root:
+            # Recupera as informações id, título e texto do XML
+            id = child[0].text
+            title = child[1].text
+            text = child[2].text
+            
+            # Passa todos os caracteres do título e do texto para maiúsculo
+            title = title.lower()
+            text = text.lower()
+            
+            # Instancia um objeto Page e atribui o id, titulo e texto da página
+            page = Page(id, title, text)
+            
+            # Junta o título com o texto para montar o hash com base em uma string só
+            words = title + ' ' + text
+            
+            # Cria uma lista de palavras contidas no título e no texto
+            words = words.split(' ')
             
             # Itera sobre cada palavra do título
             for word in words:
@@ -66,7 +97,7 @@ class Search:
         except KeyError:
             print(f"[Palavra '{word}' não encontrada no hash]")
             
-    def searchByTitleOcurrences(self, word: str) -> None:
+    def searchForRelevance(self, word: str) -> None:
         word = self.handleInput(word)
         occurrencesList = {}
         
@@ -79,13 +110,22 @@ class Search:
             for page in pages:
                 id = page.getID()
                 title = page.getTitle()
-                occurrences = title.count(word)
+                text = page.getText()
+                
+                occurrencesInTitle = title.count(word)
+                occurrencesInText = text.count(word)
+                
+                if occurrencesInTitle > 0:
+                    occurrences = occurrencesInText * 2
+                else:
+                    occurrences = occurrencesInText
+                    
                 
                 occurrencesList[page] = occurrences
 
             # Criando uma lista com os objetos ordenados por quantidade de ocorrências em ordem decrescente
             for page in sorted(occurrencesList, key = occurrencesList.get, reverse = True):
-                print(f"[{page.getID():<5}] - (Ocorrências de {word}: {occurrencesList[page]}) - {page.getTitle():<55} ")
+                print(f"[{page.getID():<5}] - (Relevância: {occurrencesList[page]}) - {page.getTitle():<55} ")
                 # sortedList.append(page)
                 
             # Printar lista de titulos ordenados por quantidade de ocorrência
@@ -94,7 +134,7 @@ class Search:
                 
         except KeyError:
             print(f"[Palavra '{word}' não encontrada no hash]")
-    
+            
     def handleInput(self, input: str) -> str:
         # Passando para minúsculo todos os caracteres contidos na palavra
         input = input.lower()
@@ -105,11 +145,11 @@ class Search:
         for palavra in self.hash:
             print(f"indice[{palavra}]: {self.hash[palavra]}")
 
+# busca = Search()
+# busca.createHash()
+# busca.searchByTitle("network")
 
-
-
-
+# Tarefa 4
 busca = Search()
-busca.createHash()
-#busca.searchByTitle("network")
-busca.searchByTitleOcurrences("education")
+busca.createHash_2()
+busca.searchForRelevance("education")
